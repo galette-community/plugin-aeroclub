1fffc18ca1e716bdc4d9b1e6dc3f7c34bef2a765
SND\MelissaD_cp <SND\MelissaD_cp@3b72ae23-dab2-410c-8069-39ac8a3268c2>
2013-07-09 21:05:42 +0000

    Correction bug 90
    git-svn-id: https://galetteaeroclub.svn.codeplex.com/svn@27193 3b72ae23-dab2-410c-8069-39ac8a3268c2

--- _define.php
+++ _define.php
@@ -32,7 +32,7 @@ $this->register(
         'Aéroclub', //Name
         'Gestion d\'un aéroclub', //Short description
         'Melissa Djebel', //Author
-        '0.8.1 / 9 mars 2013', //Version
+        '0.8.2 / 9 juillet 2013', //Version
         '0.7.4', //Galette compatible version
         null //Permissions needed - not yet implemented
 );
